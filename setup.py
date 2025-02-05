a18455ba59aa716177f539b64cdaaf8c0d7c591e
Ihar Abukhouski <ihar.abukhouski@gmail.com>
2025-02-04 21:56:13 -0600

    wip

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='llm',
+    name='browser',
     version='0.0.1',
-    packages=['llm'],
+    packages=['browser'],
     install_requires=[
         'playwright==1.50.0',
     ],
